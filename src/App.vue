96b75c8ad109e958150ab56cd187ae55b1cd2b75
Daniel Koban <danielkoban@gmail.com>
2020-12-20 13:04:59 +0100

    add Camera component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <camera></camera>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Camera from "./components/Camera.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    Camera,
   },
 };
 </script>
---
+++ src/components/Camera.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="camera">
+    <video autoplay></video>
+    <button></button>
+  </div>
+</template>
+
+<script>
+import { onMounted } from "vue";
+
+export default {
+  name: "Camera",
+  setup() {
+    function init() {
+      if (
+        "mediaDevices" in navigator &&
+        "getUserMedia" in navigator.mediaDevices
+      ) {
+        navigator.mediaDevices.getUserMedia({ video: true }).then((stream) => {
+          const videoPlayer = document.querySelector("video");
+          videoPlayer.srcObject = stream;
+          videoPlayer.play();
+        });
+      } else {
+        alert("Cannot get Media Devices");
+      }
+    }
+
+    onMounted(() => {
+      init();
+    });
+
+    return {
+      init,
+    };
+  },
+};
+</script>
+
+<style>
+</style>
