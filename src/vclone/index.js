ac16447e397fd46e725f020e1796e0bb9ecf9ca3
Daniel Koban <danielkoban@gmail.com>
2021-01-10 15:42:43 +0100

    create vuex clone

---
+++ src/vclone/index.js
@@ -0,0 +1,31 @@
+import { reactive } from 'vue';
+
+// state
+
+export const items = reactive([
+    {
+        id: 1,
+        url:
+            "https://images.pexels.com/photos/3052361/pexels-photo-3052361.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    },
+    {
+        id: 2,
+        url:
+            "https://images.pexels.com/photos/5627275/pexels-photo-5627275.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    },
+    {
+        id: 3,
+        url:
+            "https://images.pexels.com/photos/5624326/pexels-photo-5624326.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
+    },
+    {
+        id: 4,
+        url:
+            "https://images.pexels.com/photos/2224861/pexels-photo-2224861.png?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    },
+    {
+        id: 5,
+        url:
+            "https://images.pexels.com/photos/2224861/pexels-photo-2224861.png?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    },
+]);
