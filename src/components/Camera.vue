f2ca2f061c0b5418ae3a2d74d27a52698c1d47a0
Daniel Koban <danielkoban@gmail.com>
2021-01-15 21:53:48 +0100

    create SelectedItem component

--- src/components/Camera.vue
+++ src/components/Camera.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="camera w-screen h-screen fixed z-10">
     <video autoplay class="w-screen h-screen object-cover"></video>
+    <selected-item></selected-item>
     <button
       class="w-16 h-16 mx-auto absolute inset-x-0 bottom-8 rounded-full border-4 focus:outline-none"
       @click="takePicture"
@@ -10,10 +11,13 @@
 
 <script>
 import { onMounted } from "vue";
+import SelectedItem from "./SelectedItem";
 
 export default {
   name: "Camera",
-
+  components: {
+    SelectedItem,
+  },
   setup(_, context) {
     function init() {
       if (
---
+++ src/components/SelectedItem.vue
@@ -0,0 +1,16 @@
+<template>
+  <img
+    class="w-40 lg:w-80 mx-auto absolute top-36 lg:top-52 inset-x-0 z-10 absolute"
+    src="https://images.pexels.com/photos/3052361/pexels-photo-3052361.jpeg"
+    alt="Your item"
+  />
+</template>
+
+<script>
+export default {
+  name: "SelectedItem",
+};
+</script>
+
+<style>
+</style>
