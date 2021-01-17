39cb00ab520d17da169734e3e3b840b22d46f5ae
Daniel Koban <danielkoban@gmail.com>
2021-01-17 13:35:25 +0100

    add comma

--- src/vclone/index.js
+++ src/vclone/index.js
@@ -6,27 +6,27 @@ const currentItem = ref("");
 const items = reactive([
     {
         id: 1,
-        title: "The Moon over Manhattan"
+        title: "The Moon over Manhattan",
         url: "https://images.pexels.com/photos/3052361/pexels-photo-3052361.jpeg"
     },
     {
         id: 2,
-        title: "Manhattan Bridge"
+        title: "Manhattan Bridge",
         url: "https://images.pexels.com/photos/5627275/pexels-photo-5627275.jpeg"
     },
     {
         id: 3,
-        title: "The Skyscrapers"
+        title: "The Skyscrapers",
         url: "https://images.pexels.com/photos/2129796/pexels-photo-2129796.png"
     },
     {
         id: 4,
-        title: "The Empire State Building"
+        title: "The Empire State Building",
         url: "https://images.pexels.com/photos/3889873/pexels-photo-3889873.jpeg"
     },
     {
         id: 5,
-        title: "Brooklyn Bridge"
+        title: "Brooklyn Bridge",
         url: "https://images.pexels.com/photos/2346925/pexels-photo-2346925.jpeg"
     },
 ])
