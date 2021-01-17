ee503e1f320359daf90b9dae624f1d6c23ca9b83
Daniel Koban <danielkoban@gmail.com>
2021-01-17 13:12:23 +0100

    add items to components

--- src/components/Item.vue
+++ src/components/Item.vue
@@ -4,20 +4,18 @@
     :key="item.id"
     class="flex-shrink-0 w-3/12 flex justify-center items-center"
   >
-    <img :src="item.url" alt="" class="p-6" />
+    <img :src="item.url" alt="" class="p-6" @click="selectItem" />
   </li>
 </template>
 
 <script>
-import { computed } from "vue";
-import { useStore } from "vuex";
+import vclone from "@/vclone/";
 
 export default {
   setup() {
-    const store = useStore();
-    const items = computed(() => store.state.items);
+    const { items, selectItem } = vclone;
 
-    return { items };
+    return { items, selectItem };
   },
 };
 </script>
--- src/components/SelectedItem.vue
+++ src/components/SelectedItem.vue
@@ -1,14 +1,21 @@
 <template>
   <img
-    class="w-40 lg:w-80 mx-auto absolute top-36 lg:top-52 inset-x-0 z-10 absolute"
-    src="https://images.pexels.com/photos/3052361/pexels-photo-3052361.jpeg"
+    class="w-40 lg:w-80 mx-auto absolute top-28 lg:top-52 inset-x-0 z-10 absolute"
+    :src="currentItem"
     alt="Your item"
   />
 </template>
 
 <script>
+import vclone from "@/vclone/";
+
 export default {
   name: "SelectedItem",
+  setup() {
+    const { currentItem } = vclone;
+
+    return { currentItem };
+  },
 };
 </script>
 
