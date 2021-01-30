b53ddef01f6dbff93497f892c192b95fb0dca4ec
Daniel Koban <danielkoban@gmail.com>
2021-01-30 15:01:09 +0100

    add crossorigin attribute

--- src/components/SelectedItem.vue
+++ src/components/SelectedItem.vue
@@ -3,6 +3,7 @@
     class="selected-item w-40 lg:w-80 mx-auto absolute top-28 lg:top-52 inset-x-0 z-50"
     :src="currentItem"
     alt="Your item"
+    crossorigin="anonymous"
   />
 </template>
 
