326b1f1b50cc5100fe1ff4de7b2dd695a1b935c7
Daniel Koban <danielkoban@gmail.com>
2021-02-21 13:52:45 +0100

    fetch items

--- src/vclone/index.js
+++ src/vclone/index.js
@@ -3,9 +3,20 @@ import { ref } from 'vue';
 // state
 
 const currentItem = ref("");
-const items = []
+const items = ref([]);
 
 // mutations
+
+fetch("https://home-decor-api.herokuapp.com/items")
+    .then(res => res.json())
+    .then(data => {
+        items.value = data
+        console.log(data)
+    })
+    .catch((err) => {
+        alert("Sorry something went wrong. Please try later.");
+        console.log("Error: ", err);
+    })
 
 const selectItem = (e) => {
     const selectedItem = e.target.src;
