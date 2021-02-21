60e668d17023eebf700dd197b0fcdde0b00f3d34
Daniel Koban <danielkoban@gmail.com>
2021-02-21 13:54:51 +0100

    remove crossorigin attribute

--- src/components/SelectedItem.vue
+++ src/components/SelectedItem.vue
@@ -3,7 +3,6 @@
     class="selected-item w-40 lg:w-80 mx-auto absolute top-28 lg:top-52 inset-x-0 z-50"
     :src="currentItem"
     alt="Your item"
-    crossorigin="anonymous"
   />
 </template>
 
