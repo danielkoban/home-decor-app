5d4b38d9656cb048643bc4a74aef079eacebbde0
Daniel Koban <danielkoban@gmail.com>
2021-11-07 12:45:24 +0100

    add component

---
+++ src/components/DeviceInfo.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="w-scree h-screen bg-white flex flex-col justify-center items-center">
+    <h1 class="m-3 text-3xl font-bold">Device info</h1>
+    <p class="text-xl">Please use smartphone to see this section</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DeviceInfo"
+}
+</script>
+
+<style>
+
+</style>
